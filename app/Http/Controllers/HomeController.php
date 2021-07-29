a57f0c2243bc3a55424b1c198405d9275b3e013d
tamoor <tamoor.dev@gmail.com>
2021-07-29 05:45:12 -0700

    change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,7 +39,7 @@ class HomeController extends Controller
     }
 
     public function main(){
-        return view('jenkins working');
+        return view('hello i am jenking testing');
       
 
     }
